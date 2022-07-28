85ef0695ecd60573ee5e4485ed9603f765b32dec
Park-seonhyeong <Park-Seonhyeong>
2022-07-28 21:11:38 +0900

    tmp

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import cv2, glob, dlib
+global frame
+
+capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)  #웹캠을 객체로 만듭니다.
+capture.set(3, 640)  #픽셀길이 가로 640
+capture.set(4, 480)  #픽셀길이 세로 480
+print("작동")
+while True:  #'q'키를 누를 때까지 반복
+    ret, frame = capture.read()  #카메라로부터 영상 하나 읽어옵니다.
+    cv2.imshow('frame', frame)  # 영상을 window 에 표시합니다.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        img_name = "../GraduationProject/img/opencv_frame.jpg"
+        cv2.imwrite(img_name, frame)  # 영상에서 캡쳐한 이미지를 저장합니다.
+        break
 
 age_list = ['(0, 2)','(4, 6)','(8, 12)','(15, 20)','(25, 32)','(38, 43)','(48, 53)','(60, 100)']
 gender_list = ['Male', 'Female']
@@ -9,14 +22,13 @@ age_net = cv2.dnn.readNetFromCaffe(
           'models/deploy_age.prototxt', 
           'models/age_net.caffemodel')
 gender_net = cv2.dnn.readNetFromCaffe(
-          'models/deploy_gender.prototxt', 
+          'models/deploy_gender.prototxt',
           'models/gender_net.caffemodel')
 
 img_list = glob.glob('img/*.jpg')
 
 for img_path in img_list:
   img = cv2.imread(img_path)
-
   faces = detector(img)
 
   for face in faces:
@@ -37,7 +49,7 @@ for img_path in img_list:
     age_net.setInput(blob)
     age_preds = age_net.forward()
     age = age_list[age_preds[0].argmax()]
-
+    print(gender,age)
     # visualize
     cv2.rectangle(img, (x1, y1), (x2, y2), (255,255,255), 2)
     overlay_text = '%s %s' % (gender, age)
@@ -47,8 +59,38 @@ for img_path in img_list:
       fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,255,255), thickness=2)
 
   cv2.imshow('img', img)
-  cv2.imwrite('result/%s' % img_path.split('/')[-1], img)
+  cv2.imwrite('result/%s' % img_path.split('\\')[-1], img)
 
   key = cv2.waitKey(0) & 0xFF
   if key == ord('q'):
     break
+
+# def Webcam():
+#     global frame
+#     capture = cv2.VideoCapture(0)  #웹캠을 객체로 만듭니다.
+#     capture.set(3, 640)  #픽셀길이 가로 640
+#     capture.set(4, 480)  #픽셀길이 세로 480
+#
+#     while True:  #'q'키를 누를 때까지 반복
+#         ret, frame = capture.read()  #카메라로부터 영상 하나 읽어옵니다.
+#         cv2.imshow('frame', frame)  # 영상을 window 에 표시합니다.
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             img_name = "C:/Users/USER/PycharmProjects/GraduationProject/opencv_frame.jpg"
+#             cv2.imwrite(img_name, frame)  # 영상에서 캡쳐한 이미지를 저장합니다.
+#             break
+
+
+    # global frame
+    #
+    # capture_counter = 1
+    # start_time = time.time()
+
+    # while True:  #'q'키를 누를 때까지 반복
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #       img_name = "C:/Users/USER/PycharmProjects/GraduationProject/opencv_frame.jpg"
+    #       cv2.imwrite(img_name, frame)  # 영상에서 캡쳐한 이미지를 저장합니다.
+    #       break
+    #     # if time.time() - start_time >= views.leng:  #<---- (광고시간1)초 뒤에 캡쳐합니다.
+    #     #     start_time = time.time()
+    #     #     img_name = "C:/Users/USER/PycharmProjects/GraduationProject/opencv_frame.jpg"
+    #     #     cv.imwrite(img_name, frame)  #영상에서 캡쳐한 이미지를 저장합니다.
